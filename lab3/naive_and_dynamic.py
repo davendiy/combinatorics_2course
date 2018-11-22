a671cf5933db6ed81d401c76a79018db316b5548
David Zashkolny <davendiy@gmail.com>
2018-11-22 19:36:53 +0200

    lab3

--- lab3/naive_and_dynamic.py
+++ lab3/naive_and_dynamic.py
@@ -20,22 +20,66 @@ def lcs(a, b, i, j):
 def dynamic(a, b):
     m = len(a)
     n = len(b)
-    val = [[0] * n] * m
+    val = [[0 for x in range(n)] for x in range(m)]
     for i in range(m):
         for j in range(n):
-            if i == 0 or j == 0:
-                continue
             if a[i] == b[j]:
                 val[i][j] = val[i-1][j-1] + 1
             else:
                 val[i][j] = max(val[i-1][j], val[i][j-1])
-    return val[m-1][n-1] + 1
+
+    return val
+
+
+def dynamic2(a, b):
+    m = len(a)
+    n = len(b)
+    val = [[0 for i in range(n)] for i in range(m)]
+    seq = [[[] for i in range(n)] for i in range(m)]
+    for i in range(m):
+        for j in range(n):
+            if a[i] == b[j]:
+                val[i][j] = val[i-1][j-1] + 1
+                seq[i][j] = [*seq[i-1][j-1]] + [a[i]]
+            else:
+                val[i][j] = max(val[i-1][j], val[i][j-1])
+                if val[i-1][j] >= val[i][j-1]:
+                    seq[i][j] = seq[i-1][j]
+                else:
+                    seq[i][j] = seq[i][j-1]
+    # for line in val: print(line)
+    # print('\n\n')
+    # for line in seq: print(line)
+    return val, seq[m-1][n-1]
+
+
+def build_seq(matrix, a, b, i, j):
+    if i == 0 or j == 0:
+        return ''
+    elif matrix[i][j] == matrix[i-1][j-1] + 1:
+        return build_seq(matrix, a, b, i-1, j-1) + a[i]
+    elif matrix[i][j] == matrix[i][j-1]:
+        return build_seq(matrix, a, b, i, j-1)
+    elif matrix[i][j] == matrix[i-1][j]:
+        return build_seq(matrix, a, b, i-1, j)
 
 
 if __name__ == '__main__':
-    test_a = 'abdkjlkjhcacba'
-    test_b = 'backjhdl;kba'
-    print(len(test_a))
-    print(len(test_b))
-    print(dynamic(test_a, test_b))
-    print(lcs(test_a, test_b, len(test_a), len(test_b)))
+    test_a = 'absdkf'
+    test_b = 'absadflaks'
+    test_m = len(test_a)
+    test_n = len(test_b)
+
+    res = dynamic(test_a, test_b)
+    subseq = build_seq(res, test_a, test_b, test_m-1, test_n-1)
+
+    print(res[test_m-1][test_n-1])
+    print(subseq)
+
+    for row in res:
+        print(' '.join(map(str, row)))
+
+    print()
+    test2, tmp = dynamic2(test_a, test_b)
+    for row in test2:
+        print(row)
