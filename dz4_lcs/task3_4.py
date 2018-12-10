7b758b759d8c67f503932caf3b6b7b7a22856276
David Zashkolny <davendiy@gmail.com>
2018-12-10 04:30:28 +0200

    little addition

--- dz4_lcs/task3_4.py
+++ dz4_lcs/task3_4.py
@@ -6,6 +6,58 @@
 # 2 course, comp math
 # Taras Shevchenko National University of Kyiv
 # email: davendiy@gmail.com
+import functools
+
+
+def cache(func):
+    """ Decorator for save answers of any function
+    """
+    results = {}
+
+    @functools.wraps(func)
+    def __cache(*args):         # changed function
+        nonlocal results               # if this function call with parameters that already used
+        if args in results.keys():     # then answer gets from dictionary
+            # print("{} - got from cache".format(args))
+            rez = results[args]
+        else:
+            rez = func(*args)
+            results[args] = rez
+        return rez
+
+    return __cache
+
+
+@cache
+def recursive(a: tuple, i: int, j: int):
+    """ Recursive solution of the longest monotone subsequence
+    :param a: sequence (must be the tuple or any other hashable sequence for cache)
+    :param i: right bound
+    :param j: left bound
+    :return: length of the longest subsequence, subsequence
+    """
+    if i == j:                 # if right bound == left bound then sequence is one element
+        return 1, (a[i], )
+
+    _pre_count, _pre_seq = recursive(a, i-1, j)   # check if a[i] is continuation of previous max sequence
+    if a[i] >= _pre_seq[-1]:
+        return _pre_count + 1, _pre_seq + (a[i], )
+    else:
+        max_count = 1
+        max_seq = (a[i],)
+        for k in range(j, i):          # if it's false - check all sequences between i and j
+            tmp_count, tmp_seq = recursive(a, i-1, k)      # from k to i-1
+            if tmp_count+1 > max_count and a[i] >= tmp_seq[-1]:   # find maximum
+                max_count = tmp_count + 1
+                max_seq = tmp_seq + (a[i], )
+
+        for k in range(i):
+            tmp_count, tmp_seq = recursive(a, k, 0)   # and between 0 and i
+            if tmp_count+1 > max_count and a[i] >= tmp_seq[-1]:    # from 0 to k
+                max_count = tmp_count + 1
+                max_seq = tmp_seq + (a[i], )
+
+        return (max_count, max_seq) if max_count > _pre_count else (_pre_count, _pre_seq)
 
 
 def dynamic(a, b):
